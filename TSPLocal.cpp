a4cd78c492b9474e3c4798df80a7149dbbcacc4a
Bazyli Cyran <bazyli.cyran@gmail.com>
2018-10-25 17:27:23 +0200

    Add TSPLocal class for local search solvers.

---
+++ TSPLocal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bazyli on 24.10.18.
+//
+
+#include "TSPLocal.h"
---
+++ TSPLocal.h
@@ -0,0 +1,19 @@
+//
+// Created by bazyli on 24.10.18.
+//
+
+#ifndef C_TSPLOCAL_H
+#define C_TSPLOCAL_H
+
+
+#include "TSP.h"
+
+/**
+ * Provides methods for solving Travelling Salesman Problem using local search algorithms.
+ */
+class TSPLocal : public TSP {
+
+};
+
+
+#endif //C_TSPLOCAL_H
