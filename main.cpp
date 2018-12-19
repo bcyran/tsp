6365856b091a8074d6e28489a5ef0156c83c5e21
Bazyli Cyran <bazyli.cyran@gmail.com>
2018-12-19 18:03:13 +0100

    Update CLI

--- main.cpp
+++ main.cpp
@@ -2,79 +2,16 @@
 #include <functional>
 #include <iomanip>
 #include <fstream>
-#include "TimeTester.h"
+#include "TSPSimAnnealingSolver.h"
 #include "TSPTabuSolver.h"
 #include "TSPSolver.h"
 #include "TSPBFSolver.h"
 #include "TSPBnBSolver.h"
 #include "TSPDPSolver.h"
+#include <chrono>
 
 using namespace std;
-
-/**
- * Measures time of execution of each method, specified number of times for all problem sizes up to given maximum.
- *
- * @param file Output file path (CSV format).
- * @param testNum Number of tests of each method for one size.
- * @param maxSize Maximum size of the problem to measure.
- */
-void measure(const string &file, int testNum, int maxSize) {
-    // limits of size for each method to avoid too long measurements
-    int BFMax = 12;
-    int BnBMax = 18;
-    int DPMax = 23;
-
-    TSPBFSolver BFSolver;
-    TSPBnBSolver BnBSolver;
-    TSPDPSolver DPSolver;
-
-    TimeTester testers[3];
-    for (auto &tester : testers) {
-        tester.setTestNum(testNum);
-    }
-
-    ofstream output;
-    output.open(file);
-    output.imbue(locale("pl_PL.utf8"));
-    output << fixed << setprecision(12);
-    output << "Size;BF;BnB;DP" << endl;
-
-    for (int i = 1; i <= maxSize; ++i) {
-        testers[0].setPreTestFn(bind(&TSPBFSolver::random, &BFSolver, i));
-        testers[1].setPreTestFn(bind(&TSPBnBSolver::random, &BnBSolver, i));
-        testers[2].setPreTestFn(bind(&TSPDPSolver::random, &DPSolver, i));
-        output << i << ";";
-        cout << "N = " << i << endl;
-
-        if (i <= BFMax) {
-            cout << "BF:" << endl;
-            testers[0].setTestFn(bind(&TSPBFSolver::solve, &BFSolver));
-            output << testers[0].test() << ";";
-        } else {
-            output << ";";
-        }
-
-        if (i <= BnBMax) {
-            cout << "BnB:" << endl;
-            testers[1].setTestFn(bind(&TSPBnBSolver::solve, &BnBSolver));
-            output << testers[1].test() << ";";
-        } else {
-            output << ";";
-        }
-
-        if (i <= DPMax) {
-            cout << "DP:" << endl;
-            testers[2].setTestFn(bind(&TSPDPSolver::solve, &DPSolver));
-            output <<testers[2].test() << ";";
-        } else {
-            output << ";";
-        }
-
-        output << endl;
-    }
-
-    output.close();
-}
+using namespace std::chrono;
 
 /**
  * Provides REPL-like user interface for testing and using TSP algorithms.
@@ -84,6 +21,8 @@ void repl() {
     TSPBFSolver BFSolver;
     TSPBnBSolver BnBSolver;
     TSPDPSolver DPSolver;
+    TSPTabuSolver TSolver;
+    TSPSimAnnealingSolver SASolver;
 
     cout << "\nAvailable commands:\n"
             "l [file]                   - Load TSP problem from file\n"
@@ -92,7 +31,8 @@ void repl() {
             "bf                         - Brute Force algorithm\n"
             "bnb                        - Branch and Bound algorithm\n"
             "dp                         - Held-Karp algorithm (dynamic programming)\n"
-            "m [file] [tests] [size]    - Time measurements\n"
+            "t                          - Tabu Search\n"
+            "sa                         - Simulated Annealing\n"
             "q                          - Quit\n";
 
     string action;
@@ -101,6 +41,11 @@ void repl() {
     string parameter1;
     string parameter2;
     string parameter3;
+
+    Path result;
+    high_resolution_clock::time_point timeStart;
+    high_resolution_clock::time_point timeStop;
+    duration<double> time{};
 
     while (true) {
         cout << "> ";
@@ -124,26 +69,58 @@ void repl() {
         } else if (command == "bf") {
             try {
                 BFSolver.setTsp(tsp);
-                cout << BFSolver.solve().toString() << endl;
+                timeStart = high_resolution_clock::now();
+                result = BFSolver.solve();
+                timeStop = high_resolution_clock::now();
+                time = duration_cast<duration<double>>(timeStop - timeStart);
+                cout << "Solution: " << result.toString() << endl << "Duration: " << time.count() << " s" << endl;
             } catch (runtime_error &e) {
                 cerr << e.what() << endl;
             }
         } else if (command == "bnb") {
             try {
                 BnBSolver.setTsp(tsp);
-                cout << BnBSolver.solve().toString() << endl;
+                timeStart = high_resolution_clock::now();
+                result = BnBSolver.solve();
+                timeStop = high_resolution_clock::now();
+                time = duration_cast<duration<double>>(timeStop - timeStart);
+                cout << "Solution: " << result.toString() << endl << "Duration: " << time.count() << " s" << endl;
             } catch (runtime_error &e) {
                 cerr << e.what() << endl;
             }
         } else if (command == "dp") {
             try {
                 DPSolver.setTsp(tsp);
-                cout << DPSolver.solve().toString() << endl;
+                timeStart = high_resolution_clock::now();
+                result = DPSolver.solve();
+                timeStop = high_resolution_clock::now();
+                time = duration_cast<duration<double>>(timeStop - timeStart);
+                cout << "Solution: " << result.toString() << endl << "Duration: " << time.count() << " s" << endl;
             } catch (runtime_error &e) {
                 cerr << e.what() << endl;
             }
-        } else if (command == "m") {
-            measure(parameter1, stoi(parameter2), stoi(parameter3));
+        } else if (command == "t") {
+            try {
+                TSolver.setTsp(tsp);
+                timeStart = high_resolution_clock::now();
+                result = TSolver.solve();
+                timeStop = high_resolution_clock::now();
+                time = duration_cast<duration<double>>(timeStop - timeStart);
+                cout << "Solution: " << result.toString() << endl << "Duration: " << time.count() << " s" << endl;
+            } catch (runtime_error &e) {
+                cerr << e.what() << endl;
+            }
+        } else if (command == "sa") {
+            try {
+                SASolver.setTsp(tsp);
+                timeStart = high_resolution_clock::now();
+                result = SASolver.solve();
+                timeStop = high_resolution_clock::now();
+                time = duration_cast<duration<double>>(timeStop - timeStart);
+                cout << "Solution: " << result.toString() << endl << "Duration: " << time.count() << " s" << endl;
+            } catch (runtime_error &e) {
+                cerr << e.what() << endl;
+            }
         } else if (command == "q") {
             break;
         }
